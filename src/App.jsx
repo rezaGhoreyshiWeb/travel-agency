bd2cec7df0f2df2efd4d49ceecb2ae0ba5168027
rezaGhoreyshiWeb <rezaghoreyshi.contact@gmail.com>
2023-05-20 04:16:38 +0330

    Tours.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Offer from "./components/Offer";
 import Services from "./components/Services";
+import Tours from "./components/Tours";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Services/>
         <Destination/>
         <Offer/>
+        <Tours/>
       </div>
     </>
   );
