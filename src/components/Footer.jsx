3a44758ad3fcb1840b40ea55db580662f468e38e
rezaGhoreyshiWeb <rezaghoreyshi.contact@gmail.com>
2023-05-20 04:39:59 +0330

    projec finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,10 +32,10 @@ export default function Footer() {
           </div>
           <ul>
             <li>
-              <span> +0213 456 987 </span>
+              <span> +0333 666 999</span>
             </li>
             <li>
-              <span>kishansheth21@gmail.com</span>
+              <span>rezaghoreyshi.contact@gmail.com</span>
             </li>
             <li>
               <span>www.youtube.com</span>
@@ -68,7 +68,7 @@ export default function Footer() {
       </div>
       <div className="lower-footer">
         <span>
-          Copyright &copy; Travel. Designed by <a href="#">Kishan Sheth</a>
+          Copyright &copy; Travel. Designed by <a href="#">Reza Ghoreyshi</a>
         </span>
       </div>
     </Container>
